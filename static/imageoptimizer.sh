054bce6e70254b5ef28c5aa544a451fd7cd48155
Stephen Ajulu <alunje73@gmail.com>
2021-10-27 11:44:31 +0300

    svg optmization + font weight 400

--- static/imageoptimizer.sh
+++ static/imageoptimizer.sh
@@ -2,3 +2,5 @@
 rsync -a images/ .imgbackup
 find images/ *.png -exec optipng {} \;
 find images/ *.{jpeg,jpg} -exec jpegoptim {} \;
+find images/ *.gif -exec gifsicle --batch --optimize {} \;
+find images/ *.svg -exec svgo {} \;
